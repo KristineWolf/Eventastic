6e4ca1b0925477f894c72f2d9fba96d52ebdfd7d
KristineWolf <kristine_wolf@web.de>
2016-08-29 18:02:05 +0200

    einige Codespassagen, die immer wieder auftauchen wurden zusammengefasst in einer Klasse mit statischen Methoden

--- app/src/main/java/com/example/kristine/eventastic/Activities/AddEvent.java
+++ app/src/main/java/com/example/kristine/eventastic/Activities/AddEvent.java
@@ -20,6 +20,7 @@ import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.kristine.eventastic.Databases.ExternDatabase;
+import com.example.kristine.eventastic.JavaClasses.ChangeDateFormat;
 import com.example.kristine.eventastic.JavaClasses.Event;
 import com.example.kristine.eventastic.R;
 /**
@@ -138,39 +139,12 @@ public class AddEvent extends AppCompatActivity implements AdapterView.OnItemSel
     }
 
     private void addEvent(String city, String date, String time, String titel, String definition, String type) {
-
-        Date dueDate = getDateFromString(date);
-        GregorianCalendar cal = new GregorianCalendar();
-        cal.setTime(dueDate);
-        String month;
-        String day;
-        if (cal.get(Calendar.MONTH) + 1 < 10) {
-            month = "" + 0 + (cal.get(Calendar.MONTH) + 1);
-        } else {
-            month = "" + (cal.get(Calendar.MONTH) + 1);
-        }
-        if (cal.get(Calendar.DAY_OF_MONTH) < 10) {
-            day = "" + 0 + cal.get(Calendar.DAY_OF_MONTH);
-        } else {
-            day = "" + cal.get(Calendar.DAY_OF_MONTH);
-        }
-        int realDate = Integer.parseInt("" + cal.get(Calendar.YEAR) + month + day);
-        Event event = new Event(city, realDate, time, titel, definition, type);
+        Event event = new Event(city, ChangeDateFormat.changeFirstIntoDateFormatAfterwardsIntoInteger(date), time, titel, definition, type);
         boolean saved = db.insertItem(event);
 
         Toast.makeText(this,"You added the Event '"+titel+"'.",Toast.LENGTH_LONG).show();
     }
 
-    private Date getDateFromString(String dateString) {
-        DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT,
-                Locale.GERMANY);
-        try {
-            return df.parse(dateString);
-        } catch (ParseException e) {
-
-            return new Date();
-        }
-    }
 
     private void initUI() {
         initTypeSpinner();
--- app/src/main/java/com/example/kristine/eventastic/JavaClasses/ChangeDateFormat.java
+++ app/src/main/java/com/example/kristine/eventastic/JavaClasses/ChangeDateFormat.java
@@ -1,5 +1,12 @@
 package com.example.kristine.eventastic.JavaClasses;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
 /**
  * Created by Kristine on 29.08.2016.
  */
@@ -18,5 +25,36 @@ public class ChangeDateFormat {
         String month=""+date.charAt(4)+date.charAt(5);
         String year =""+date.charAt(0)+date.charAt(1)+date.charAt(2)+date.charAt(3);
         return ""+day+"."+month+"."+year;
+    }
+
+    private static Date getDateFromString(String dateString){
+        DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT,
+                Locale.GERMANY);
+        try {
+            return df.parse(dateString);
+        } catch (ParseException e) {
+
+            return new Date();
+        }
+    }
+
+    public static int changeFirstIntoDateFormatAfterwardsIntoInteger(String date){
+        Date dueDate = getDateFromString(date);
+        GregorianCalendar cal = new GregorianCalendar();
+        cal.setTime(dueDate);
+        String month;
+        String day;
+        if (cal.get(Calendar.MONTH) + 1 < 10) {
+            month = "" + 0 + (cal.get(Calendar.MONTH) + 1);
+        } else {
+            month = "" + (cal.get(Calendar.MONTH) + 1);
+        }
+        if (cal.get(Calendar.DAY_OF_MONTH) < 10) {
+            day = "" + 0 + cal.get(Calendar.DAY_OF_MONTH);
+        } else {
+            day = "" + cal.get(Calendar.DAY_OF_MONTH);
+        }
+        return Integer.parseInt("" + cal.get(Calendar.YEAR) + month + day);
+
     }
 }
