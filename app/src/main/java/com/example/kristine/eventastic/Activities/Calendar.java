32f6e6016e348bce325be9348967210ac6208054
KristineWolf <kristine_wolf@web.de>
2016-09-04 18:35:32 +0200

    von calendar nun zu allevents

--- app/src/main/java/com/example/kristine/eventastic/Activities/Calendar.java
+++ app/src/main/java/com/example/kristine/eventastic/Activities/Calendar.java
@@ -100,7 +100,7 @@ public class Calendar extends AppCompatActivity {
         nextEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Calendar.this,AllPossibleCities.class);
+                Intent intent=new Intent(Calendar.this,AllEvents.class);
                 startActivity(intent);
             }
         });
