d1292c620f7907ee9f82c0d39d27730a2eb7aaa3
KristineWolf <kristine_wolf@web.de>
2016-09-28 03:11:55 +0200

    auskommentiert

--- app/src/main/java/com/example/kristine/eventastic/Interface/LocationUpdateListener.java
+++ app/src/main/java/com/example/kristine/eventastic/Interface/LocationUpdateListener.java
@@ -2,9 +2,7 @@ package com.example.kristine.eventastic.Interface;
 
 import android.location.Location;
 
-/**
- * Created by Kristine on 11.09.2016.
- */
+
 public interface LocationUpdateListener {
 
         public void onNewLocation(Location location);
