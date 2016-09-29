c9bb563752d9782a379de89248cc6441cba79dcc
Teresa <teresa.then@gmx.de>
2016-09-30 00:27:24 +0200

    update

--- app/src/main/java/com/example/kristine/eventastic/JavaClasses/AlertReceiver.java
+++ app/src/main/java/com/example/kristine/eventastic/JavaClasses/AlertReceiver.java
@@ -1,5 +1,6 @@
 package com.example.kristine.eventastic.JavaClasses;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -8,7 +9,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
 
 import com.example.kristine.eventastic.Activities.CalendarActivity;
 import com.example.kristine.eventastic.Activities.ParticipatingEvents;
@@ -17,16 +17,14 @@ import com.example.kristine.eventastic.R;
 
 public class AlertReceiver extends BroadcastReceiver{
 
+
     @Override
     public void onReceive(Context context, Intent intent) {
-
-
 
         Intent notificationIntent = new Intent(context,CalendarActivity.class);
         notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
         Uri sound = Uri.parse("android.resource://" +"com.example.kristine.eventastic.JavaClasses"+ "/" + R.raw.xylophone);
-        long[] vibrate = { 0, 100, 200, 300 };
 
         // setup actions
         Intent calendarIntent = new Intent(context, CalendarActivity.class);
@@ -45,15 +43,18 @@ public class AlertReceiver extends BroadcastReceiver{
                         .setContentTitle(context.getResources().getString(R.string.notification_title))
                         .setContentText(context.getResources().getString(R.string.notification_text))
                         .setSound(sound)
-                        .setVibrate(vibrate)
                         .setLights(Color.RED, 3000, 3000)
                         .setStyle(new NotificationCompat.BigTextStyle().bigText(context.getResources().getString(R.string.notification_text_large)))
                         .setAutoCancel(true)
                         .addAction(action1)
                         .addAction(action2);
 
+        Notification notification = mBuilder.build();
+        notification.flags = Notification.DEFAULT_LIGHTS | Notification.FLAG_AUTO_CANCEL | Notification.DEFAULT_VIBRATE;
+
         // gets an instance of the NotificationManager service and builds notification for upcomming event
         NotificationManager myNotificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-        myNotificationManager.notify(1,mBuilder.build());
+        myNotificationManager.notify(1,notification);
+
     }
 }
