70be66c8e63f9301e290f7f6007bc4582991fc9e
Teresa <teresa.then@gmx.de>
2016-09-29 23:22:22 +0200

    notification verändert

--- app/src/main/java/com/example/kristine/eventastic/JavaClasses/AlertReceiver.java
+++ app/src/main/java/com/example/kristine/eventastic/JavaClasses/AlertReceiver.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 
 import com.example.kristine.eventastic.Activities.CalendarActivity;
 import com.example.kristine.eventastic.R;
@@ -17,29 +18,37 @@ public class AlertReceiver extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
 
+
+
         Intent notificationIntent = new Intent(context,CalendarActivity.class);
         notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
         Uri sound = Uri.parse("android.resource://" +"com.example.kristine.eventastic.JavaClasses"+ "/" + R.raw.xylophone);
         long[] vibrate = { 0, 100, 200, 300 };
-        PendingIntent intentToCalendar = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+        // setup actions
+        Intent calendarIntent = new Intent(context, CalendarActivity.class);
+        PendingIntent piCalendar = PendingIntent.getActivity(context,1,calendarIntent,PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Action action1 = new NotificationCompat.Action.Builder(R.drawable.calendar, context.getResources().getString(R.string.calendar), piCalendar).build();
+
+        Intent eventsIntent = new Intent(context, CalendarActivity.class);
+        PendingIntent piEvents = PendingIntent.getActivity(context,1,eventsIntent,PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Action action2 = new NotificationCompat.Action.Builder(R.drawable.to_all_events, context.getResources().getString(R.string.participating_events), piEvents).build();
+
+
+        // setup notification
         android.support.v4.app.NotificationCompat.Builder mBuilder =
                 new android.support.v4.app.NotificationCompat.Builder(context)
-                        .setSmallIcon(R.drawable.to_all_events)
+                        .setSmallIcon(R.drawable.fox)
                         .setContentTitle(context.getResources().getString(R.string.notification_title))
                         .setContentText(context.getResources().getString(R.string.notification_text))
                         .setSound(sound)
                         .setVibrate(vibrate)
                         .setLights(Color.RED, 3000, 3000)
-                        .setContentIntent(intentToCalendar);
-        android.support.v4.app.NotificationCompat.InboxStyle inboxStyle = new android.support.v4.app.NotificationCompat.InboxStyle();
-        String[] events = new String[6];
-        inboxStyle.setBigContentTitle("Hope you have a great Time.");
-        for (int i=0; i < events.length; i++) {
-            inboxStyle.addLine(events[i]);
-        }
-        mBuilder.setAutoCancel(true);
-        mBuilder.setStyle(inboxStyle);
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText(context.getResources().getString(R.string.notification_text_large)))
+                        .setAutoCancel(true)
+                        .addAction(action1)
+                        .addAction(action2);
 
         // gets an instance of the NotificationManager service and builds notification for upcomming event
         NotificationManager myNotificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
