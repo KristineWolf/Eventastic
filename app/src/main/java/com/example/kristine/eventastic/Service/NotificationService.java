f638769055208d3b0e8e9f00b184f0bfdedd4f02
Teresa <teresa.then@gmx.de>
2016-09-29 20:59:49 +0200

    variable übergeben

--- app/src/main/java/com/example/kristine/eventastic/Service/NotificationService.java
+++ app/src/main/java/com/example/kristine/eventastic/Service/NotificationService.java
@@ -65,7 +65,7 @@ public class NotificationService extends Service {
 
 
             Intent intent = new Intent(getApplicationContext(),AlertReceiver.class);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(),1,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(),i,intent,PendingIntent.FLAG_UPDATE_CURRENT);
             AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                 alarmManager.setExact(AlarmManager.RTC_WAKEUP,notificationInMillisec,pendingIntent);
